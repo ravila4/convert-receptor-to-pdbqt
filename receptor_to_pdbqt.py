ce9a341a5d8c7a1c8e6d161f593871f72e53e344
ravila4 <ravila@protonmail.com>
2018-02-11 19:27:24 -0700

    Initial commit.

---
+++ receptor_to_pdbqt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Ricardo Avila
+# Script to convert receptors to pdbqt using ODDT
+
+import sys
+import oddt
+from oddt.docking.AutodockVina import write_vina_pdbqt
+
+if len(sys.argv) != 4:
+    print("Usage:", sys.argv[0], "<format> <receptor> <output_folder>")
+
+else:
+    file_type = sys.argv[1]
+    receptor_file = sys.argv[2]
+    path = sys.argv[3]
+
+    # Read input molecule
+    receptor = next(oddt.toolkit.readfile(file_type, receptor_file))
+    
+    # Write output 
+    write_vina_pdbqt(receptor, path, flexible=False)
